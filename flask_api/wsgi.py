6317f9838781b1d081acf0a78f83343253a870df
Audrey Andoy <audrey@Audreys-MacBook-Air.local>
2021-02-02 00:12:50 -0800

    production version TODO

--- flask_api/wsgi.py
+++ flask_api/wsgi.py
@@ -1,7 +1,7 @@
-import api
-handler = api.create_app()
+import app
+handler = app.create_app()
 
 if __name__ == '__main__':
     # Entry point when run via Python interpreter.
     print("== Running in debug mode ==")
-    api.create_app().run(host='0.0.0.0', port=8080, debug=True)
+    handler.run(host='0.0.0.0', port=8080, debug=True)
